72f6589c424f3b5ec8c2fde8905c59353bf4b081
Nhat Quang Tran <trannhatquang1104@gmail.com>
2019-10-15 11:33:55 -0400

    Create MotionControl module and MouseControl module

--- Assets/Quang/Scripts/PlayerController.cs
+++ Assets/Quang/Scripts/PlayerController.cs
@@ -1,15 +1,4 @@
-﻿// wrr world
-
-
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using UnityEngine;
-using UnityEngine.UI;
-using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using UnityEngine;
 using EZCameraShake;
 
 
@@ -20,17 +9,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public float speed;
     public float rotationSpeed;
-
     private int count;
-    Thread receiveThread;
-    UdpClient client;
-    int port;
-    float upForce;
-    float rightForce;
-    Vector3 controlVec;
-    Vector3 leftVec;
-    Vector3 rightVec;
-    // dummy comment
+    Controller controller;
+    bool useMotionControl;
 
     public GameObject player;
 
@@ -38,63 +19,22 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
-        port = 5065;
-        InitUDP();
-        leftVec = new Vector3(0f, 0f, 0f);
-        rightVec = new Vector3(0f, 0f, 0f);
+        useMotionControl = false;
+
+        controller = initializeController(useMotionControl);
     }
 
-    // 3. InitUDP
-    private void InitUDP()
+    private Controller initializeController(bool useMotionControl)
     {
-        print("UDP Initialized");
-        receiveThread = new Thread(new ThreadStart(ReceiveData));
-        receiveThread.IsBackground = true;
-        receiveThread.Start();
-    }
-
-    // 4. Receive Data
-    private void ReceiveData()
-    {
-        client = new UdpClient(port);
-        while (true)
+        if (useMotionControl)
         {
-            try
-            {
-                IPEndPoint anyIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), port);
-                byte[] data = client.Receive(ref anyIP);
-                string text = Encoding.UTF8.GetString(data);
-                print(text);
-                UpdateLeftRightVec(text);
-            }
-            catch (Exception e)
-            {
-                print(e.ToString());
-            }
+            return null;
+        }
+        else
+        {
+            return new MouseControl();
         }
     }
-
-    void UpdateLeftRightVec(string signal)
-    {
-        var cords = signal.Split(',');
-        
-        var left = cords[0].Split(' ');
-        var right = cords[1].Split(' ');
-        try
-        {
-            leftVec = new Vector3(float.Parse(left[0]), float.Parse(left[1]), 0f);
-            rightVec = new Vector3(float.Parse(right[0]), float.Parse(right[1]), 0f);
-
-            float mag = (leftVec - rightVec).magnitude;
-            Vector3 vec = (leftVec + rightVec) / 2;
-            vec.z = mag;
-        }
-        catch (Exception e)
-        {
-            print(e.ToString());
-        }
-    }
-
 
     void FixedUpdateQuang()
     {
@@ -107,45 +47,25 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(move * speed);
     }
 
-    Vector3 GetRotation()
-    {
-        Vector3 vec = (leftVec + rightVec) / 2;
-        Vector3 leftRight = rightVec - leftVec;
-        float angle;
-        if (leftRight.magnitude == 0f)
-        {
-            angle = 0f;
-        }
-        else {
-            angle = Vector3.SignedAngle(new Vector3(1f, 0f, 0f), leftRight, new Vector3(0f, 0f, 1f));
-        }
-        vec.z = angle / 360;
-        return vec;
-    }
-
-    float GetSpeed()
-    {
-        return (leftVec - rightVec).magnitude;
-    }
-
     void Update()
     {
         // mouse control
-        Vector3 rotate = Input.mousePosition;
-        float speedMag = 1f;
-        rotate.x = (rotate.x - Screen.width / 2) / Screen.width;
-        rotate.y = (rotate.y - Screen.height / 2) / Screen.height;
-        rotate.z = 0f;
-    
+        //Vector3 rotate = Input.mousePosition;
+        //float speedMag = 1f;
+        //rotate.x = (rotate.x - Screen.width / 2) / Screen.width;
+        //rotate.y = (rotate.y - Screen.height / 2) / Screen.height;
+        //rotate.z = 0f;
 
+        Vector3 rotate = controller.GetRotation();
+        float speedMag = controller.GetSpeed();
 
         // hand control
         // Vector3 rotate = GetRotation();
         // float speedMag = GetSpeed();
 
 
-
-        player.transform.Rotate(-rotate.y * Time.deltaTime * rotationSpeed, rotate.x*Time.deltaTime*rotationSpeed, rotate.z*Time.deltaTime*rotationSpeed, Space.Self);
+        
+        player.transform.Rotate(-rotate.y * Time.deltaTime * rotationSpeed, rotate.x * Time.deltaTime * rotationSpeed, rotate.z * Time.deltaTime * rotationSpeed, Space.Self);
         player.transform.position += speed * player.transform.forward * Time.deltaTime * speedMag * 1f;
 
         if (Input.GetKeyDown("v"))
