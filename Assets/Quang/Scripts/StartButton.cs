51311ae9f2966aae97155ba31fb3be4f736a08c9
Quang Nhat Tran <trannhatquang1104@gmail.com>
2019-10-23 16:34:56 -0400

    Added start button

---
+++ Assets/Quang/Scripts/StartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+
+    public GameObject PauseAtStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseAtStart.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ButtonPressed()
+    {
+        PauseAtStart.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
