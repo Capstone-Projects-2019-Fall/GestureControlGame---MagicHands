b311b7e3f342d6a5dd9d9bf2520174c870458e1c
Quang Nhat Tran <trannhatquang1104@gmail.com>
2019-10-28 23:25:08 -0400

    decouple motion control

--- Assets/Quang/Scripts/MotionControlIntuitive.cs
+++ Assets/Quang/Scripts/MotionControlIntuitive.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 using System.Drawing;
 using System.Diagnostics;
 
-public class MotionControl : ControllerQuang
+public class MotionControlIntuitive : ControllerQuang
 {
 
     Thread receiveThread;
@@ -24,16 +24,16 @@ public class MotionControl : ControllerQuang
     Vector3 leftCenter;
     Vector3 rightCenter;
 
-    public MotionControl()
+    public MotionControlIntuitive()
     {
         port = 5065;
         InitUDP();
         EllapsedTime = 0;
-        stopwatch = new Stopwatch();
         leftCenter = new Vector3(-0.25f, 0f, 0f);
         rightCenter = new Vector3(0.25f, 0f, 0f);
         leftVec = leftCenter;
         rightVec = rightCenter;
+        stopwatch = new Stopwatch();
     }
 
     // 3. InitUDP
@@ -56,7 +56,7 @@ public class MotionControl : ControllerQuang
                 IPEndPoint anyIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), port);
                 byte[] data = client.Receive(ref anyIP);
                 string text = Encoding.UTF8.GetString(data);
- 
+
                 UpdateLeftRightVec(text);
             }
 
@@ -79,7 +79,7 @@ public class MotionControl : ControllerQuang
         Vector3 vec = (leftVec + rightVec) / 2 * 30f;
         Point cursorPos = new Point();
         GetCursorPos(out cursorPos);
-        SetCursorPos(cursorPos.X + (int) Math.Ceiling(vec.x), cursorPos.Y - (int) Math.Ceiling(vec.y));
+        SetCursorPos(cursorPos.X + (int)Math.Ceiling(vec.x), cursorPos.Y - (int)Math.Ceiling(vec.y));
     }
 
     void UpdateLeftRightVec(string signal)
@@ -92,21 +92,16 @@ public class MotionControl : ControllerQuang
         {
             if (right[0].Equals(noVal))
             {
-                if (left[0].Equals(noVal)) // no value at all
-                {
-                    return;
-                }
-                else // 1 value
-                {
-                    rightVec = new Vector3(float.Parse(left[0]), float.Parse(left[1]), 0f);
-                    leftVec = leftCenter;
-                }
+                return;
             }
             else // 2 values
             {
                 leftVec = new Vector3(float.Parse(left[0]), float.Parse(left[1]), 0f);
                 rightVec = new Vector3(float.Parse(right[0]), float.Parse(right[1]), 0f);
             }
+
+            leftVec = new Vector3(float.Parse(left[0]), float.Parse(left[1]), 0f);
+            rightVec = new Vector3(float.Parse(right[0]), float.Parse(right[1]), 0f);
         }
         catch (Exception e)
         {
@@ -117,15 +112,25 @@ public class MotionControl : ControllerQuang
 
     public override Vector3 GetRotation()
     {
-        Vector3 vec = rightVec - rightCenter;
-        vec.z = - (leftVec - leftCenter).x;
-        return vec*30f;
+        Vector3 vec = (leftVec + rightVec) / 2;
+        Vector3 leftRight = rightVec - leftVec;
+        float angle;
+        if (leftRight.magnitude == 0f)
+        {
+            angle = 0f;
+        }
+        else
+        {
+            angle = Vector3.SignedAngle(new Vector3(1f, 0f, 0f), leftRight, new Vector3(0f, 0f, 1f));
+        }
+        vec.z = angle / 360;
+        return vec * 30f;
     }
 
     public override float GetSpeed()
     {
-        Vector3 vec = leftVec - leftCenter;
-        return vec.y + 0.5f;
+        float speed = (leftVec - rightVec).magnitude;
+        return speed;
     }
 
     [DllImport("user32.dll")]
