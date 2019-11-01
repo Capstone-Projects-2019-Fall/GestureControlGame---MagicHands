86fbfd74b3e17754afdbd00675a078fdde0be593
Jin Yau <tug20073@temple.edu>
2019-11-01 00:54:21 -0400

    Added start voice control

--- Assets/Quang/Scripts/StartButton.cs
+++ Assets/Quang/Scripts/StartButton.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+using System;
 
 public class StartButton : MonoBehaviour
 {
    
     public GameObject PauseAtStart;
+    
+    private Dictionary<string, Action> keyActs = new Dictionary<string, Action>();
+
+    private KeywordRecognizer recognizer;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        keyActs.Add("start", ButtonPressed);
+
+        recognizer = new KeywordRecognizer(keyActs.Keys.ToArray());
+        recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        recognizer.Start();
         PauseAtStart.SetActive(true);
         Time.timeScale = 0f;
+    }
+
+    void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Command: " + args.text);
+        keyActs[args.text].Invoke();
     }
 
     public void ButtonPressed()
