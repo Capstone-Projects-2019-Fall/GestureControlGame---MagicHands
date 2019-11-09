54573c0022d3c022c8db60921d10b0f2e6309d6d
zahego <tug70814@temple.edu>
2019-11-09 17:05:15 -0500

    main scene is weird, but new win-lose scene, plus lap system

---
+++ Assets/Quang/Scripts/LapSystem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LapSystem : MonoBehaviour
+{
+    int counter;
+    bool isWait;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+        isWait = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*psedocode
+         * on contacting the gate 3 times
+         * invoke QuitGame()
+         * */
+        if (counter == 3)
+        {
+            QuitGame();
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (isWait == false)
+            {
+                counter++;
+                StartCoroutine(WaitTime());
+            }
+        }
+
+    }
+    public void QuitGame()
+    {
+        
+            Time.timeScale = 1f;
+            WinLose.isWin = true;
+            SceneManager.LoadScene("WinLose");
+        
+    }
+    IEnumerator WaitTime()
+    {
+        isWait = true;
+        yield return new WaitForSeconds(3);
+        isWait = false;
+        
+
+    }
+}
