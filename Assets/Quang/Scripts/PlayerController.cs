cb622b346cda0fce088400f8ee19e7026ed5cbd8
Mykhailo <tug83611@temple.edu>
2019-11-09 21:24:58 -0500

    2 players only, version with IsMine

--- Assets/Quang/Scripts/PlayerController.cs
+++ Assets/Quang/Scripts/PlayerController.cs
@@ -10,8 +10,12 @@ using Photon.Pun;
 public class PlayerController : MonoBehaviour
 {
     //this is test message
-
+    //mp
     private PhotonView PV;
+    private CharacterController myCC;
+    //*mp
+
+
     private Rigidbody rb;
     public ParticleSystem warp;
     public ParticleSystem flame;
@@ -20,7 +24,7 @@ public class PlayerController : MonoBehaviour
     private int count;
     //Controller controller;
     //public GameObject gameManager;
-    public GameObject player;
+    //public GameObject player;
     public static bool speedBoostState = false;
     //GameManager gameManagerCode;
     Vector3 PrevPos; 
@@ -35,7 +39,12 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        //mp
         PV = GetComponent<PhotonView>();
+        myCC = GetComponent<CharacterController>();
+
+        //*mp
+
         flame.Clear();
         flame.Stop();
         warp.Clear();
@@ -52,7 +61,7 @@ public class PlayerController : MonoBehaviour
         recognizer.Start();
         //gameManagerCode = gameManager.GetComponent<GameManager>();
     }
-    void FixedUpdate()
+    /*void FixedUpdate()
     {
         float xMove = Input.GetAxis("Horizontal");
         float zMove = Input.GetAxis("Vertical");
@@ -62,7 +71,7 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(move * speed);
 
-    }
+    }*/
     void OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
         Debug.Log("Command: " + args.text);
@@ -84,44 +93,44 @@ public class PlayerController : MonoBehaviour
         if (PV.IsMine)
         {
             if (!GameManager.started)
-        {
-            return;
-        }
-        Vector3 rotate = GameManager.controller.GetRotation();
-        float speedMag = GameManager.controller.GetSpeed();
-
-        rotate = rotate * Time.deltaTime * rotationSpeed;
-        
-        player.transform.Rotate(-rotate.y, rotate.x, rotate.z, Space.Self);
-        player.transform.position += speedMultiplier * speed * player.transform.forward * Time.deltaTime * speedMag;
-        
-
-        if (Input.GetKeyDown("v"))
-        {
-            CameraShaker.Instance.ShakeOnce(10f, 10f, .5f, 1.5f);
-        }
-        if (Input.GetKeyDown("space"))
-        {
-            if (speedBoostState == true)
             {
-                StartCoroutine(SpeedBoost());
+               return;
             }
-        }
-        if (Input.GetKeyDown("p"))
-        {
-            speedMultiplier = 0f;
-        }
-        if (Input.GetKeyUp("p"))
-        {
-            speedMultiplier = 1f;
-        }
-        NewPos = transform.position;  // each frame track the new position
-        //Debug.Log("delta time:" + Time.deltaTime);
-        //Debug.Log("fixed delta time:" + Time.fixedDeltaTime);
-        ObjVelocity = (NewPos - PrevPos) / Time.deltaTime;  // velocity = dist/time
-        PrevPos = NewPos;  // update position for next frame calculation
-       
-            
+            Vector3 rotate = GameManager.controller.GetRotation();
+            float speedMag = GameManager.controller.GetSpeed();
+
+            rotate = rotate * Time.deltaTime * rotationSpeed;
+        
+            transform.Rotate(-rotate.y, rotate.x, rotate.z, Space.Self);
+            transform.position += speedMultiplier * speed *transform.forward * Time.deltaTime * speedMag;
+        
+
+            if (Input.GetKeyDown("v"))
+            {
+                CameraShaker.Instance.ShakeOnce(10f, 10f, .5f, 1.5f);
+            }
+            if (Input.GetKeyDown("space"))
+            {
+                if (speedBoostState == true)
+                {
+                    StartCoroutine(SpeedBoost());
+                }
+            }
+            if (Input.GetKeyDown("p"))
+            {
+                speedMultiplier = 0f;
+            }
+            if (Input.GetKeyUp("p"))
+            {
+             speedMultiplier = 1f;
+            }
+            NewPos = transform.position;  // each frame track the new position
+                                      //Debug.Log("delta time:" + Time.deltaTime);
+                                      //Debug.Log("fixed delta time:" + Time.fixedDeltaTime);
+            ObjVelocity = (NewPos - PrevPos) / Time.deltaTime;  // velocity = dist/time
+            PrevPos = NewPos;  // update position for next frame calculation
+
+
         }
     
     }
---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,208 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class RoomController : MonoBehaviourPunCallbacks, IInRoomCallbacks
+{
+    //room info
+    public static RoomController room;
+    private PhotonView PV;
+
+    public bool isGameLoaded;
+    public int currentScene;
+
+    static Player[] playerList;
+    public int playersInRoom;
+    public int myNumberInRoom;
+
+    public int playerInGame;
+    //delays
+  
+    private bool readyToStart;
+    public float startingTime;
+   
+    private float atMaxPlayers;
+    private float timeToStart;
+
+
+
+    public GameObject myAvatar;
+
+
+    private void Awake()
+    {
+        //set up singleton
+        if (RoomController.room == null)
+        {
+            RoomController.room = this;
+        }
+        else
+        {
+            if (RoomController.room != this)
+            {
+                Destroy(RoomController.room.gameObject);
+                RoomController.room = this;
+            }
+
+
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        PhotonNetwork.AddCallbackTarget(this);
+        SceneManager.sceneLoaded += OnSceneFinishedLoading;
+    }
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        PhotonNetwork.RemoveCallbackTarget(this);
+        SceneManager.sceneLoaded -= OnSceneFinishedLoading;
+    }
+
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+      
+        readyToStart = false;
+       
+        atMaxPlayers = 3;
+        timeToStart = startingTime;
+    }
+
+
+    void Update()
+    {
+        if (MultyplayerSettings.multyplayerSettings.delayStart)
+        {
+           
+            if (!isGameLoaded)
+            {
+                if (readyToStart)
+                {
+
+                    atMaxPlayers -= Time.deltaTime;
+                    
+                    timeToStart = atMaxPlayers;
+                }
+               
+                if (timeToStart <= 0)
+                {
+                    StartGame();
+                }
+            }
+        }
+
+    }
+    public override void OnJoinedRoom() //Callback function for when we successfully create or join a room.
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Room joined");
+        playerList = PhotonNetwork.PlayerList;
+        playersInRoom = PhotonNetwork.PlayerList.Length;
+        myNumberInRoom = playersInRoom;
+        PhotonNetwork.NickName = myNumberInRoom.ToString();
+        if (MultyplayerSettings.multyplayerSettings.delayStart)
+        {
+            Debug.Log("Players in room (" + playersInRoom + ":" + MultyplayerSettings.multyplayerSettings.maxPlayers + ")");
+
+            if (playersInRoom == MultyplayerSettings.multyplayerSettings.maxPlayers)
+            {
+                readyToStart = true;
+                if (!PhotonNetwork.IsMasterClient)
+                    return;
+                PhotonNetwork.CurrentRoom.IsOpen = false;
+            }
+        }
+        else
+        {
+            StartGame();
+        }
+
+    }
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        Debug.Log("New player has entered room");
+        playerList = PhotonNetwork.PlayerList;
+        playersInRoom++;
+        if (MultyplayerSettings.multyplayerSettings.delayStart)
+        {
+            Debug.Log("Players in room (" + playersInRoom + ":" + MultyplayerSettings.multyplayerSettings.maxPlayers + ")");
+
+            if (playersInRoom == MultyplayerSettings.multyplayerSettings.maxPlayers)
+            {
+                readyToStart = true;
+                if (!PhotonNetwork.IsMasterClient)
+                    return;
+                PhotonNetwork.CurrentRoom.IsOpen = false;
+            }
+
+
+
+        }
+    }
+
+
+    void StartGame() //Function for loading into the multiplayer scene.
+    {
+        Debug.Log("Starting Game");
+        isGameLoaded = true;
+
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+        if (MultyplayerSettings.multyplayerSettings.delayStart)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+
+
+        }
+        PhotonNetwork.LoadLevel(MultyplayerSettings.multyplayerSettings.multyplayerScene); //because of AutoSyncScene all photonPlayers who join the room will also be loaded into the multiplayer scene.
+    }
+  
+    void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+
+        currentScene = scene.buildIndex;
+        if (currentScene == MultyplayerSettings.multyplayerSettings.multyplayerScene)
+        {
+            isGameLoaded = true;
+            if (MultyplayerSettings.multyplayerSettings.delayStart)
+            {
+                PV.RPC("RPC_LoadedGameScene", RpcTarget.MasterClient);
+            }
+            else
+            {
+                RPC_CreatePlayer();
+            }
+        }
+
+
+    }
+    [PunRPC]
+    private void RPC_LoadedGameScene()
+    {
+        playerInGame++;
+        if (playerInGame == PhotonNetwork.PlayerList.Length)
+        {
+            PV.RPC("RPC_CreatePlayer", RpcTarget.All);
+
+        }
+    }
+    [PunRPC]
+    private void RPC_CreatePlayer()
+    {
+        int spawnSelector = UnityEngine.Random.Range(0, GameSetup.GS.spawnPoints.Length);
+        //PhotonNetwork.Instantiate(Path.Combine("Prefab", "TempEnemy"), transform.position, Quaternion.identity, 0);
+        //PhotonNetwork.Instantiate("Assets/Prefab/TempEnemy", transform.position, Quaternion.identity, 0);
+        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "NetworkPlayer2"), transform.position, Quaternion.identity, 0);
+        
+    }
+
+
+}
