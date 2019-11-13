96b7c448785772f15cd8b2db1206118b3cc3a6c1
Jin Yau <tug20073@temple.edu>
2019-11-13 02:08:26 -0500

    Test scene

---
+++ Assets/Quang/Scripts/CreateRoomMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Text _roomName;
+    
+    public void OnClick_CreateRoom()
+    {
+        if(!PhotonNetwork.IsConnected)
+            return;
+            
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom(_roomName.text, options, TypedLobby.Default);
+        
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created Room succesfully.", this);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed:" + message, this);
+    }
+}
---
+++ Assets/Quang/Scripts/RoomListingMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class RoomListingMenu : MonoBehaviourPunCallbacks
+{
+   [SerializeField]
+   private Transform _content;
+   [SerializeField]
+   private RoomListing _roomListing;
+
+   private List<RoomListing> _listings = new List<RoomListing>();
+   
+   public override void OnRoomListUpdate(List<Photon.Realtime.RoomInfo> roomList)
+   {
+        foreach (RoomInfo info in roomList)
+        {
+            if(info.RemovedFromList)
+            {
+
+            }
+            else
+            {
+                RoomListing listing = (RoomListing)Instantiate(_roomListing, _content);
+                if (listing != null)
+                {
+                    listing.SetRoomInfo(info);
+                    _listings.Add(listing);
+                }
+
+            }
+        }
+   }
+}
