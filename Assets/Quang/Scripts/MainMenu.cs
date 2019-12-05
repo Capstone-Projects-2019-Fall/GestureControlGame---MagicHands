349ccd9e3ea580c430912c7e26e8b36e74d7163e
Jin Yau <tug20073@temple.edu>
2019-12-05 01:11:46 -0500

    added input username

--- Assets/Quang/Scripts/MainMenu.cs
+++ Assets/Quang/Scripts/MainMenu.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public static string photonNickname;
+
+    
+    public Text nickname;
+
+    void Start()
+    {
+  
+    }
 
     public void Multyplayer()
     {
@@ -38,12 +49,14 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
 
     }
+    public void SetUsername()
+    {
+        photonNickname = nickname.text.ToString();
+        print(photonNickname);
+    }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
